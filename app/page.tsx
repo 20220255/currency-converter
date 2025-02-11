0db973c784901ce9dd46efe7fe4092a421a3f934
20220255 <118017744+20220255@users.noreply.github.com>
2025-02-12 01:07:38 +0800

    Modify title tab and description

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <div suppressHydrationWarning className="container">
-        {/* <h1>Currency Converter</h1> */}
         <CurrencyConverter baseCurrency="USD" />
       </div>
     </Suspense>
