6327f3f4da46b8844f3d3c3d13e4c6614f265104
20220255 <118017744+20220255@users.noreply.github.com>
2025-02-24 00:20:01 +0800

    Update Cypress

--- cypress/e2e/currency-exchange-test.cy.ts
+++ cypress/e2e/currency-exchange-test.cy.ts
@@ -3,13 +3,13 @@
 describe('template spec', () => {
   it('passes', () => {
     const currency = 'GBP';
-    // const regexPattern = new RegExp(`.\\d+.\\d\\d USD is equal to .\\d+.\\d\\d ${currency}`);
+    const regexPattern = new RegExp(`.\\d+.\\d\\d USD is equal to .\\d+.\\d\\d ${currency}`);
     cy.visit('/')
     cy.get('label')
       .contains('To:')
       .next()
       .select(currency)
     cy.get('.convert-button').click()
-    cy.get('.result').contains(/USD/i).should('be.visible')
+    cy.get('.result').contains(regexPattern).should('be.visible')
   })
 })
