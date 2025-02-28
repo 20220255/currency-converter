19c6a8e5dcf5c476823eb9902dac0c4c70e3ffed
20220255 <118017744+20220255@users.noreply.github.com>
2025-02-28 19:59:25 +0800

    Added test data

--- cypress/e2e/currency-exchange-test.cy.ts
+++ cypress/e2e/currency-exchange-test.cy.ts
@@ -1,12 +1,20 @@
 /// <reference types="cypress" />
 
 import CurrencyConverterPage from "../support/pageObjects/currencyConverter"
+import testData from '../testData/testData.js'
 
 describe('Execute Curreny Exchange', () => {
   it('should run the currency exchange', () => {
-    const currencyConverterPage = new CurrencyConverterPage();
-    cy.visit('/');
-    const regexPattern = currencyConverterPage.executeConversion('100', 'PHP', 'EUR');
-    currencyConverterPage.resultLabel.contains(regexPattern).should('be.visible');
+    testData.forEach((data) => {
+      const currencyConverterPage = new CurrencyConverterPage();
+      cy.visit('/');
+      const regexPattern = currencyConverterPage.executeConversion(data.amount, data.fromCurrency, data.toCurrency);
+      currencyConverterPage.resultLabel.contains(regexPattern).should('be.visible');
+    })
   });
+    // const currencyConverterPage = new CurrencyConverterPage();
+    // cy.visit('/');
+    // const regexPattern = currencyConverterPage.executeConversion('100', 'PHP', 'EUR');
+    // currencyConverterPage.resultLabel.contains(regexPattern).should('be.visible');
+  // });
 })
---
+++ cypress/testData/testData.js
@@ -0,0 +1,19 @@
+const testData = [
+    {
+        "fromCurrency": "USD",
+        "toCurrency": "EUR",
+        "amount": "100"
+    },
+    {
+        "fromCurrency": "PHP",
+        "toCurrency": "EUR",
+        "amount": "200"
+    },
+    {
+        "fromCurrency": "JPY",
+        "toCurrency": "EUR",
+        "amount": "INR"
+    },
+]
+
+export default testData;
