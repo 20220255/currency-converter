52fbefccbc2858b609dd737d532a6fe03d55dde8
20220255 <118017744+20220255@users.noreply.github.com>
2025-03-01 20:47:50 +0800

    Removed button object from the test

--- cypress/e2e/currency-exchange-test.cy.ts
+++ cypress/e2e/currency-exchange-test.cy.ts
@@ -1,17 +1,14 @@
 /// <reference types="cypress" />
 
-import { Button } from "../support/Objects/classObjects";
 import CurrencyConverterPage from "../support/pageObjects/currencyConverter"
 import testData from '../testData/testData.js'
 
 describe('Execute Curreny Exchange', () => {
   it('should run the currency exchange', () => {
-    const btnConvert =  new Button("Convert");
     const currencyConverterPage = new CurrencyConverterPage();
-
+    
     testData.forEach((data) => {
       cy.visit('/');
-      btnConvert.click();
       const regexPattern = currencyConverterPage.executeConversion(data.amount, data.fromCurrency, data.toCurrency);
       currencyConverterPage.resultLabel.contains(regexPattern).should('be.visible');
     })
